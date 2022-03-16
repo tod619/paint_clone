d10f48838c8466f8d37a1b53e25da2627c4e330b
tod619 <trevorodwyer619@hotmail.com>
2022-03-16 20:44:05 +0000

    set brush + bg color, + eraser + switchToBrush function in script.js file

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const context = canvas.getContext('2d')
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
-// let isEraser = false;
+let isEraser = false;
 // let isMouseDown = false;
 // let drawnArray = [];
 
@@ -34,34 +34,37 @@ let currentColor = '#A51DAB';
 // });
 
 // Setting Brush Color
-// brushColorBtn.addEventListener('change', () => {
-
-// });
+brushColorBtn.addEventListener('change', () => {
+  isEraser = false
+  currentColor = `#${brushColorBtn.value}`
+});
 
 // Setting Background Color
-// bucketColorBtn.addEventListener('change', () => {
+bucketColorBtn.addEventListener('change', () => {
+  bucketColor = `#${bucketColorBtn.value}`
+  createCanvas()
+});
 
-// });
+// Eraser
+eraser.addEventListener('click', () => {
+  isEraser = true
+  brushIcon.style.color = 'white';
+  eraser.style.color = 'black';
+  activeToolEl.textContent = 'Eraser';
+  currentColor = bucketColor
+  currentSize = 50
+});
 
-// // Eraser
-// eraser.addEventListener('click', () => {
+// Switch back to Brush
+function switchToBrush() {
+  isEraser = false;
+  activeToolEl.textContent = 'Brush';
+  brushIcon.style.color = 'black';
+  eraser.style.color = 'white';
+  currentColor = `#${brushColorBtn.value}`;
+  currentSize = 10;
 
-//   brushIcon.style.color = 'white';
-//   eraser.style.color = 'black';
-//   activeToolEl.textContent = 'Eraser';
-
-// });
-
-// // Switch back to Brush
-// function switchToBrush() {
-//   isEraser = false;
-//   activeToolEl.textContent = 'Brush';
-//   brushIcon.style.color = 'black';
-//   eraser.style.color = 'white';
-//   currentColor = `#${brushColorBtn.value}`;
-//   currentSize = 10;
-
-// }
+}
 
 // Create Canvas
 function createCanvas() {
@@ -70,7 +73,7 @@ function createCanvas() {
   context.fillStyle = bucketColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
   body.appendChild(canvas);
-
+  switchToBrush()
 }
 
 // // Clear Canvas
@@ -194,8 +197,8 @@ canvas.addEventListener('mouseup', () => {
 //   setTimeout(switchToBrush, 1500);
 // });
 
-// // Event Listener
-// brushIcon.addEventListener('click', switchToBrush);
+// Event Listener
+brushIcon.addEventListener('click', switchToBrush);
 
 // On Load
 createCanvas();
